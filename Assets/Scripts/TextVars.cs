75abeddd31b9356e4d5052fd81608f4a6d5ee43b
Sardijas <63937050+Sardijas@users.noreply.github.com>
2025-01-26 13:43:42 -0500

    minor changes

--- Assets/Scripts/TextVars.cs
+++ Assets/Scripts/TextVars.cs
@@ -11,7 +11,7 @@ public class TextVars : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        var sm = GameObject.Find("StatMan");               
+        var StatMan sm = GameObject.Find("StatMan");               
         teasText.text = "Teas Drank: " + sm.numTeasDrank.ToString();
         enemiesText.text = "Enemies Defeated: " + sm.enemiesKilled.ToString();
         moneyText.text = "Money Obtained: " + sm.totalMoney.ToString();
