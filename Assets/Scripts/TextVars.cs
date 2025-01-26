57152ebd5a42d54d2b196c5e339a4c7a64187405
Sardijas <63937050+Sardijas@users.noreply.github.com>
2025-01-26 13:05:22 -0500

    End Scene first draft

---
+++ Assets/Scripts/TextVars.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextVars : MonoBehaviour
+{
+    public GameObject teasText;
+    public GameObject enemiesText;
+    public GameObject moneyText;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        var sm = GameObject.Find("StatMan");               
+        teasText.text = "Teas Drank: " + sm.numTeasDrank.ToString();
+        enemiesText.text = "Enemies Defeated: " + sm.enemiesKilled.ToString();
+        moneyText.text = "Money Obtained: " + sm.totalMoney.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
